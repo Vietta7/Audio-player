58a1712177c1ba5df8af7505b0ee89ff1680b877
Anna Belova <annaspacer7@gmail.com>
2024-09-09 00:55:40 +0300

    feat: add functional for volume

--- audio-player/js/main.js
+++ audio-player/js/main.js
@@ -23,3 +23,33 @@ playPauseBtn.addEventListener('click', function() {
         playPauseIcon.alt = 'play'; 
     }
 });
+
+
+// Управление звуком
+
+const volume = document.getElementById('volume');
+
+audio.volume = volume.value;
+volume.addEventListener('input', function() {
+    audio.volume = volume.value;
+});
+
+const soundToggle = document.getElementById('sound-toggle');
+const volumeIcon = document.getElementById('volume-icon');
+
+let isMuted = false;
+
+soundToggle.addEventListener('click', function() {
+    if (isMuted) {
+        audio.muted = false;
+        volumeIcon.src = './img/icon/volume-up.png';
+        volumeIcon.alt = 'sound-on';
+        alert('Звук включен');
+    } else {
+        audio.muted = true;
+        volumeIcon.src = './img/icon/sound-off.png';
+        volumeIcon.alt = 'sound-off';
+        alert('Звук выключен');
+    }
+    isMuted = !isMuted; 
+});
